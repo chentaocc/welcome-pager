466f33d93b74448534e843172363f2591b7afedd
chentao <chent.sy@star-net.cn>
2020-04-23 14:01:03 +0800

    Initial Commit

---
+++ app/src/main/java/com/welcom/lm/welcompage/util/SharedPreferencesUtil.java
@@ -0,0 +1,32 @@
+package com.welcom.lm.welcompage.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by liumce on 2018/1/11
+ */
+public class SharedPreferencesUtil {
+
+    private static final String spFileName = "welcomePage";
+    public static final String FIRST_OPEN = "first_open";
+
+    public static Boolean getBoolean(Context context, String strKey,
+                                     Boolean strDefault) {
+        //strDefault	boolean: Value to return if this preference does not exist.
+        SharedPreferences setPreferences = context.getSharedPreferences(
+                spFileName, Context.MODE_PRIVATE);
+        Boolean result = setPreferences.getBoolean(strKey, strDefault);
+        return result;
+    }
+
+    public static void putBoolean(Context context, String strKey,
+                                  Boolean strData) {
+        SharedPreferences activityPreferences = context.getSharedPreferences(
+                spFileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = activityPreferences.edit();
+        editor.putBoolean(strKey, strData);
+        editor.commit();
+    }
+
+}
---
+++ app/src/test/java/com/welcom/lm/welcompage/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.welcom.lm.welcompage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by liumce on 2018/1/11
+ */
+
+public class ExampleUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
